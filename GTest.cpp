d237845c0e4ab3318363aaf966ce2ef4695a510f
Frank <frank.vossen.1966@gmail.com>
2024-12-08 15:02:44 +0100

    Put in GTest.cpp + CMakeListst.txt

---
+++ GTest.cpp
@@ -0,0 +1,21 @@
+#include <gtest/gtest.h>
+
+// Code under test
+int Add(int a, int b) {
+    return a + b;
+}
+
+// Test cases
+TEST(AddTest, HandlesPositiveNumbers) {
+    EXPECT_EQ(Add(1, 2), 3);
+}
+
+TEST(AddTest, HandlesNegativeNumbers) {
+    EXPECT_EQ(Add(-1, -2), -3);
+}
+
+// Main function for Google Test
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
